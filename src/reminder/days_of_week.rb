e887419e6b514031bb92438e5eb0f7343586e18e
AlexBaranosky <alexander.baranosky@gmail.com>
2010-09-24 00:25:13 -0400

    tweaked hash in DaysOfWeek

--- src/reminder/days_of_week.rb
+++ src/reminder/days_of_week.rb
@@ -7,17 +7,14 @@ class DaysOfWeek
 
   private
 
+  WDAYS = { :sundays => 0, :mondays => 1, :tuesdays => 2, :wednesdays => 3, :thursdays => 4, :fridays => 5, :saturdays => 6 }
+  WDAYS.default = nil
+
   def specified_time_point?(date)
-    valid_time_points.map { |d| day_of_week(d) }.include?(date.wday)
+    valid_time_points.map { |d| WDAYS[d] }.include?(date.wday)
   end
 
   def any_invalid_time_points?(day_syms)
-    day_syms.any? { |d| day_of_week(d) == nil }
-  end
-
-  def day_of_week(sym)
-    wday = { :sundays => 0, :mondays => 1, :tuesdays => 2, :wednesdays => 3, :thursdays => 4, :fridays => 5, :saturdays => 6 }
-    wday.default = nil
-    wday[sym]
+    day_syms.any? { |d| WDAYS[d] == nil }
   end
 end
