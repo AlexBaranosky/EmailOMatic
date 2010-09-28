ee51bc05865c38fa973b528a9b69bdb5a9dada9e
AlexBaranosky <alexander.baranosky@gmail.com>
2010-09-27 21:31:10 -0400

    modified persistent email records to make more a more human-readable records file.  Refactored the code as well.

--- specs/reminder/persistent_email_records_spec.rb
+++ specs/reminder/persistent_email_records_spec.rb
@@ -9,17 +9,17 @@ def yesterday; today - 1 end
 
 describe PersistentEmailRecords do
   it 'should know the number of reminders already sent today' do
-    records = PersistentEmailRecords.new(fake_records_file(today))
+    records = PersistentEmailRecords.new(fake_records_file today)
     records.num_reminders_already_sent_today.should === NUM_REMINDERS_ALREADY_SENT_TODAY
   end
 
   it "should reset the number of reminders already sent to 0 on each new day" do
-    records = PersistentEmailRecords.new(fake_records_file(yesterday))
+    records = PersistentEmailRecords.new(fake_records_file yesterday)
     records.num_reminders_already_sent_today.should === 0
   end
 
   it "should record a reminders' count and day of persisting" do
-    records = PersistentEmailRecords.new(fake_records_file(yesterday))
+    records = PersistentEmailRecords.new(fake_records_file yesterday)
     records.record_num_of_reminders_and_todays_wday_value([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
     records.num_reminders_already_sent_today.should === 12
   end
@@ -29,7 +29,7 @@ def fake_records_file(wday)
   file_name = File.dirname(__FILE__) + '/../../resources/fake_persistent_email_records.txt'
 
   File.open(file_name, 'w') do |f|
-    f.write("#{NUM_REMINDERS_ALREADY_SENT_TODAY}\n#{wday}")
+    f.write("NUM_REMINDERS_SENT_ALREADY_TODAY: #{NUM_REMINDERS_ALREADY_SENT_TODAY}\nLAST_PERSIST_WDAY: #{wday}")
   end
   file_name
 end
--- src/reminder/persistent_email_records.rb
+++ src/reminder/persistent_email_records.rb
@@ -2,33 +2,32 @@ require 'date'
 require File.dirname(__FILE__) + '/../../src/general/enumerable_monkey_patch'
 
 class PersistentEmailRecords
-  def initialize(num_records_sent_records_file = File.dirname(__FILE__) + '/../../resources/persistent_email_records.txt')
-    @num_records_sent_records_file = num_records_sent_records_file
+  def initialize(records_file = File.dirname(__FILE__) + '/../../resources/persistent_email_records.txt')
+    @records_file = records_file
   end
 
   def num_reminders_already_sent_today
     return 0 if its_a_new_day?
-    persisted_value_of_num_of_reminders_last_sent
+    recorded_value_of("NUM_REMINDERS_SENT_ALREADY_TODAY")
   end
 
 
   def record_num_of_reminders_and_todays_wday_value(due_reminder_list)
-    File.open(@num_records_sent_records_file, 'w') do |f|
-      f.write("#{due_reminder_list.size}\n#{today}\n")   
+    File.open(@records_file, 'w') do |f|
+      record =  "NUM_REMINDERS_SENT_ALREADY_TODAY: #{due_reminder_list.size}" << "\n"
+      record << "LAST_PERSIST_WDAY: #{today}" << "\n"
+      f.write(record)
     end
   end
 
   private
 
-  def its_a_new_day?; persisted_value_of_wday_of_last_persist_time != today end
-
-  def today; DateTime.now.wday end
-
-  def persisted_value_of_num_of_reminders_last_sent
-    File.open(@num_records_sent_records_file).first  { |line| line }.to_i
+  def its_a_new_day?
+    recorded_value_of("LAST_PERSIST_WDAY") != DateTime.now.wday
   end
 
-  def persisted_value_of_wday_of_last_persist_time
-    File.open(@num_records_sent_records_file).second { |line| line }.to_i
+  def recorded_value_of(line_marker)
+    File.open(@records_file).find { |line| line =~ /#{line_marker}:\s*(\d+)/ }
+    $1.to_i
   end
 end
