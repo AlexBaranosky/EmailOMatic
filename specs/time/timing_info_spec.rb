79b7c2dbf1252373be73b78515d25e6e1f49a948
Alex Baranosky <alexander.baranosky@gmail.com>
2010-10-19 23:42:27 -0400

    fixed timinginfo spec

--- specs/time/timing_info_spec.rb
+++ specs/time/timing_info_spec.rb
@@ -1,9 +1,10 @@
 require 'rspec'
+require 'date'
 require File.dirname(__FILE__) + '/../../src/time/timing_info'
 
 describe TimingInfo do
   it 'should tell the next date time in the future' do
-    timing_info = TimingInfo.new([DateTime.parse("2004/4/1"), DateTime.parse("2004/5/1")])
-    timing_info.next_time.should == DateTime.parse("2004/4/1")
+    timing_info = TimingInfo.new([DateTime.parse("3000/4/1"), DateTime.parse("3000/5/1")])
+    timing_info.next_time.should == DateTime.parse("3000/4/1")
   end
 end
