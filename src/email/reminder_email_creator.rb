9df6ea17fc348e7fdd939bcd904b3dc052f00a26
AlexBaranosky <alexander.baranosky@gmail.com>
2010-10-24 14:16:15 -0400

    further refactoring of the EmailCreating code

--- src/email/reminder_email_creator.rb
+++ src/email/reminder_email_creator.rb
@@ -4,20 +4,20 @@ class ReminderEmailCreator
   FROM    = %Q|"EmailOMatic Reminder Service" <>|
 
   def create_email(recipient, reminders)
-    body   = create_email_body(reminders)
-
-    header = <<MESSAGE_END
-From:     #{FROM}
-To: "    #{recipient.name}    " <    #{recipient.email_address}    >
-Subject: EmailOMatic Reminder Service
-MESSAGE_END
-
-    header + body
+    header_for(recipient) + email_body_for(reminders)
   end
 
   private
 
-  def create_email_body(reminders)
+  def header_for(recipient)
+    <<MESSAGE_END
+From:     #{FROM}
+To: "    #{recipient.name}    " <    #{recipient.email_address}    >
+Subject: EmailOMatic Reminder Service
+MESSAGE_END
+  end
+
+  def email_body_for(reminders)
     "The following reminders are coming up for tomorrow and the day after tomorrow: \n\n" +
         reminders.map.with_index do |reminder, idx|
           "#{idx + 1}. #{reminder}"
