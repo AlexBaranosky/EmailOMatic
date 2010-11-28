d638f3740ed906f04cbc88caf8a8a2b611b0bbec
AlexBaranosky <alexander.baranosky@gmail.com>
2010-11-28 17:08:24 -0500

    now sends out an email if a CannotParseException is raised during parsing of the reminders.

---
+++ specs/email_o_matic_spec.rb
@@ -0,0 +1,6 @@
+require File.dirname(__FILE__) + '/test_helpers'
+require File.dirname(__FILE__) + '/../src/email_o_matic'
+
+describe EmailOMatic do
+  it "should send email to recipients with the error message and stacktrace in it, when the reminers are in some way unparsable"
+end
